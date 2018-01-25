edaa265b7fbbbcc02a94aa8b281d233452d60a5b
Danielturcan <15dturcan@stjosephsrush.com>
2018-01-25 10:44:25 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,14 @@
 from time import sleep
 while True:
 
-    print("try guess my age")
+    print("try guess this persons age bet you wont get it")
     sleep(2)
-    choice=input("Guess my age!")
+    choice=input("Guess his age!")
     if choice=="15":
         sleep(1)
-        print("you are correct")
+        print("you are correct welldone")
         break
     else:
         sleep(1)
-        print("your wrong")
+        print("your wrong m8")
+        
